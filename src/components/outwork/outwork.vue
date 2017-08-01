798e1f737b53a96f74e00b3fac3b0b7ea43fa2aa
mjfxdd <1062144788@qq.com>
2017-08-01 14:22:24 +0800

    qjf

--- src/components/outwork/outwork.vue
+++ src/components/outwork/outwork.vue
@@ -1,53 +1,148 @@
 <template>
-  <div class="outwork">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+  <!--<div class="navheader">人员清点</div>-->
+  <el-row class="menu_title_wrap">
+    <el-col :span="1" style="height:10px"></el-col>
+    <el-col :span="22">
+      <div class="li1_parts">
+        <div class="tabHead">
+          <div class="tab">出工</div>
+          <div class="tab tabing">留监</div>
+        </div>
+        <div class="partsBody">
+          <div class="bodyCon">
+            <el-col :span="2">
+              <div class="criminal">
+                <img src="../../assets/crimal_1_03.jpg" width="98%" height="85" alt=""/>
+                <span class="criminalName">张三</span>
+              </div>
+            </el-col>
+
+
+          </div>
+          <el-row >
+            <el-col :span="8" style="height: 10px"></el-col>
+            <el-col :span="8" >
+              <div class="pages">
+                <span class="pageControl"><img src="../../assets/q1.png" alt=""/></span>
+                <span class="pagesText">11/30</span>
+                <span class="pageControl"><img src="../../assets/q2.png" alt=""/></span>
+              </div>
+            </el-col>
+            <el-col :span="8" style="height: 10px"></el-col>
+          </el-row>
+
+        </div>
+        <div class="partsFoot">
+          <div class="sure">收工</div>
+        </div>
+      </div>
+    </el-col>
+    <el-col :span="1"  style="height:10px"></el-col>
+  </el-row>
+
 </template>
 
 <script>
-export default {
-  name: 'outwork',
-  data () {
-    return {
-      msg: '这是个outwork'
-    }
+  export default {
+    name: 'navheader'
   }
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+<style>
+  .li1_parts{
+    height: 740px;
+    margin: 0px auto;
+  }
+  .li1_parts .tabHead{
+    width: 100%;
+    height: 40px;
+  }
+  .li1_parts .tab{
+    width: 127px;
+    height: 40px;
+    background: white;
+    font-size: 18px;
+    color: #004bdc;
+    text-align: center;
+    float: left;
+    line-height: 38px;
+  }
+  .li1_parts .tabing{
+    background: #004bdc;
+    font-size: 18px;
+    color:white;
+    text-align: center;
+  }
+  .li1_parts .partsBody{
+    width: 100%;
+    height: 614px;
+    background: white;
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  }
+  .li1_parts .partsFoot{
+    width: 100%;
+    height: 84px;
+    background: #c5cfdb;
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  }
+  .li1_parts .bodyCon{
+    height: 522px;
+    padding: 20px;
+  }
+  .li1_parts .pages{
+    width: 273px;
+    height: 50px;
+    margin: 0px auto;
+  }
+  .li1_parts .pageControl{
 
-a {
-  color: #42b983;
-}
+  }
+  .li1_parts .pagesText {
+    font-size: 24px;
+    display: inline-block;
+    width: 155px;
+    text-align: center;
+  }
+  .li1_parts .sure{
+    width: 126px;
+    height: 40px;
+    background: #004bdc;
+    text-align: center;
+    color: white;
+    font-size: 20px;
+    line-height: 36px;
+    letter-spacing: 9px;
+    float: right;
+    margin: 24px 95px;
+  }
+
+
+
+  .li1_parts .criminal{
+    /*width: 64px;*/
+    height: 110px;
+    /*float: left;*/
+    background: #0048d9;
+    margin: 6px 6px;
+    text-align: center;
+    padding: 2px;
+  }
+  .li1_parts .criminal img{
+    border: 1px solid white;
+  }
+  .li1_parts .criminalName{
+    font-size: 12px;
+    color: white;
+    display: inline-block;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    width: 99%;
+  }
+
+  .navheader{
+    height: 122px;
+    color: #fff;
+    font-size: 20px;
+  }
 </style>
